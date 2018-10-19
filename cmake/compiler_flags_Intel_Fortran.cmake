7323de8fb1712e6c5b61eddd220d922f85064e55
Rahul Mahajan <aerorahul@users.noreply.github.com>
2018-10-19 15:24:47 -0400

    use ecbuild
    Change-Id: Ic8d973483df92c66efe61cace48c659c6f634c15

---
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -0,0 +1,53 @@
+# (C) Copyright 2018 UCAR.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+####################################################################
+# FLAGS COMMON TO ALL BUILD TYPES
+####################################################################
+
+if( HAVE_OMP )
+  set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp")
+else( )
+  set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp-stubs")
+endif( )
+
+####################################################################
+# BASE FLAGS (used by both RELEASE and DEBUG)
+####################################################################
+
+set( CMAKE_Fortran_FLAGS_BASE "-ftz -align all -fno-alias -traceback -r8 -stack_temps -safe_cray_ptr -assume byterecl -fp-model source -convert big_endian -fPIC -fpe0 -heap-arrays 32 -assume noold_maxminloc -align dcommons")
+
+####################################################################
+# RELEASE FLAGS
+####################################################################
+
+set( CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_BASE} -O3 -qopt-report0 -qno-offload" )
+
+####################################################################
+# DEBUG FLAGS
+####################################################################
+
+#set( CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_BASE} -g -O0 -debug -nolib-inline -fno-inline-functions -assume protect_parens,minus0 -prec-div -prec-sqrt -check bounds -check uninit -fp-stack-check -ftrapuv -warn unused -init=snan,arrays" )
+
+set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -r8 -g -check bounds -traceback -warn -heap-arrays -fpe-all=0 -fpe:0 -check assume -check format -check output_conversion -check pointers -check stack -check uninit" )
+
+####################################################################
+# BIT REPRODUCIBLE FLAGS
+####################################################################
+
+set( CMAKE_Fortran_FLAGS_BIT     "-O2 -ip -ipo -unroll -inline -no-heap-arrays" )
+
+####################################################################
+# LINK FLAGS
+####################################################################
+
+set( CMAKE_Fortran_LINK_FLAGS    "" )
+
+####################################################################
+
+# Meaning of flags
+# ----------------
+# todo
+
